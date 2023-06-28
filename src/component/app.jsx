cc1f193a223b54b84a65af3c8231838108781e0e
Nchang Denzel <93846700+FlyntDenzel@users.noreply.github.com>
2023-06-28 08:47:21 +0100

    created header and footer for keeper app

--- src/component/app.jsx
+++ src/component/app.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Heading from "./heading"
-import List from "./list";
+import Header from "./header";
+import Note from "./note";
 
 function App() {
-    return<div>
-        <Heading />
-        <List />
+    return <div>
+        <Header />
+        <Note />
     </div>
 }
 
